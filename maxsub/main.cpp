3d040782b5902bd44680a27365141b62a7f3359e
sacrefies <jasonqiaomeng@outlook.com>
2016-02-15 13:24:29 -0500

    initial content submit for max subarray program with only framework.

---
+++ maxsub/main.cpp
@@ -0,0 +1,24 @@
+// ------------------------------------------------------------------------------------
+//       Filename: main.cpp
+//       Revision: $Id$
+//    Description: This file contains the main program execution entrance.
+//        Created: 02/15/2016 01:15:59 PM
+//       Compiler: G++
+//         Author: Jason Meng (jm), jm652564@sju.edu
+//
+// Copyright (c) 2016 by Jason Meng
+// ------------------------------------------------------------------------------------
+
+
+// C includes
+#include <stdlib.h>
+// C++ includes
+#include <iostream>
+// custom includess
+
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
+
