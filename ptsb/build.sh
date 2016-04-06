c8b789573d39e0f80f8883e0757e6b3d293cd909
Jason Meng <jasonqiaomeng@outlook.com>
2016-04-06 10:51:38 -0400

    added build script for linux

--- ptsb/build.sh
+++ ptsb/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -std=c++11 -O3 -o ptsp.exe brutes.cpp logging.cpp utilities.cpp main.cpp -Wall -O3 -DGRAD -DEXTRA_CREDIT_RECURSIVE -DEXTRA_CREDIT_NON_RECURSIVE
+g++ -std=c++11 -O3 -o ptsp brutes.cpp logging.cpp utilities.cpp main.cpp -Wall -O3 -DGRAD -DEXTRA_CREDIT_RECURSIVE -DEXTRA_CREDIT_NON_RECURSIVE
 echo "DONE"
 echo "The runnable: ./main"
