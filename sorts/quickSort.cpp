1b311b2d8101c588cfe7501b893f07829ec3fbe2
jm652564 <jasonqiaomeng@outlook.com>
2017-05-10 22:47:27 -0400

    added random permutation for partition

--- sorts/quickSort.cpp
+++ sorts/quickSort.cpp
@@ -2,9 +2,13 @@
 // Created by Jason on 5/10/2017.
 //
 
+#include <random>
 #include "headers/sorts.h"
-#include "headers/timing.h"
 
+
+using std::random_device;
+using std::mt19937_64;
+using std::uniform_int_distribution;
 
 /// The partition function which split and rearrange list[start, end]
 /// into L[start, pivot] and R[pivot+1, end]
@@ -34,6 +38,24 @@ static int partition(int list[], int startIndex, int endIndex) {
     return i + 1;
 }
 
+/// Random sampling.
+/// Instead of always partitioning using A[r], randomly swap A[r] w/ an element of A[p..r] first.
+/// \param list The array to be partitioned
+/// \param startIndex The array's start index
+/// \param endIndex The array's end index.
+/// \return The index of the pivot which splits the array.
+static int randomizedPartion(int list[], int startIndex, int endIndex) {
+    random_device rd;
+    mt19937_64 gen(rd());
+    uniform_int_distribution<int> dist(startIndex, endIndex);
+    int r = dist(gen);
+    // swap list[r] and list[endIndex]
+    int tmp = list[r];
+    list[r] = list[endIndex];
+    list[endIndex] = tmp;
+    return partition(list, startIndex, endIndex);
+}
+
 /// The quick sort inner helper function
 /// \param list The array to be sorted.
 /// \param startIndex The start index of the array.
@@ -41,7 +63,7 @@ static int partition(int list[], int startIndex, int endIndex) {
 static void innerQuickSort(int list[], int startIndex, int endIndex) {
     // the base case - it's already sorted
     if (startIndex >= endIndex) return;
-    int pivotIndex = partition(list, startIndex, endIndex);
+    int pivotIndex = randomizedPartion(list, startIndex, endIndex);
     // the left sub-array:
     innerQuickSort(list, startIndex, pivotIndex - 1);
     // the right sub-array
